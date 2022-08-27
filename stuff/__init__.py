640ee6c38a95d6dddf42633e7c5e8aa4a45dfdaf
kranthi-panda <106347485+kranthi-panda@users.noreply.github.com>
2022-08-27 19:02:47 +0530

    commit test cases chapter 5

---
+++ stuff/__init__.py
@@ -0,0 +1,1 @@
+#dunder init file
---
+++ tests/test_accum.py
@@ -0,0 +1,50 @@
+"""
+This module contains basic unit tests for accum operations.
+Their purpose is to show how to use the pytest framework by example.
+
+Terminal > pytest
+"""
+
+#----------------------------------------------------------------------
+# imports
+#----------------------------------------------------------------------
+import pytest
+from stuff.accum import Accumulator
+
+#----------------------------------------------------------------------
+# Tests
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+# Best Practice: Pattern to follow while writing test cases
+# Arrange
+# Act
+# Assert
+#----------------------------------------------------------------------
+
+def test_accumulator_init():
+    accum = Accumulator()
+    assert accum.count == 0
+
+def test_accumulator_add_one():
+    accum = Accumulator()       # Arrange: construct accum object
+    accum.add()                 # Act: calls accum object ref
+    assert accum.count == 1     # Assert: verify count/error
+
+def test_accumulator_add_three():
+    accum = Accumulator()
+    accum.add(3)
+    assert accum.count == 3
+
+def test_accumulator_add_twice():
+    accum = Accumulator()
+    accum.add()
+    accum.add()
+    assert accum.count == 2
+
+def test_accumulator_cannot_set_count_directly():
+    accum = Accumulator()
+    with pytest.raises(AttributeError, match=r"can't set attribute") as e:
+        accum.count = 10
+
+
